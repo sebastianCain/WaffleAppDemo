e3058c3be9c973606f4c28f10dd01d47b92558ed
Sebastian Cain <sebi.cain@yahoo.com>
2015-05-27 17:37:27 -0400

    Initial Commit

---
+++ WaffleAppDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WaffleAppDemo
+//
+//  Created by Sebastian Cain on 5/27/15.
+//  Copyright (c) 2015 Sebastian Cain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WaffleAppDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WaffleAppDemo
+//
+//  Created by Sebastian Cain on 5/27/15.
+//  Copyright (c) 2015 Sebastian Cain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
